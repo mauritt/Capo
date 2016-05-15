6207c1e62842b6bb30d06a877150d59a25ab637c
Michael <michael@michaelauritt.com>
2016-05-15 16:39:42 -0400

    Added Capogiro location info

---
+++ capo_locations.py
@@ -0,0 +1,29 @@
+locations = {}
+
+locations['13th Street'] = {
+                                'number': '1',
+                                'street address': '119 South 13th Street',
+                                'zipcode': '19107',
+                                'phone': '215-351-0900',
+                                }
+
+locations['Rittenhouse'] = {
+                                'number': '2',
+                                'street address': '117 South 20th Street',
+                                'zipcode': '19103',
+                                'phone': '215-636-9250',
+                                }
+
+locations['CapoYunk'] = {
+                                'number': '3',
+                                'street address': '1625 East Passyunk Ave',
+                                'zipcode': '19148',
+                                'phone': '215-462-3790',
+                                }
+
+locations['CapoPenn'] = {
+                                'number': '4',
+                                'street address': '3925 Walnut Street',
+                                'zipcode': '19104',
+                                'phone': '215-222-0252',
+                                }
