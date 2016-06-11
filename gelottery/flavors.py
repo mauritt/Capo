8b88f0983490f6b6334a55b95736e4070883abee
Michael <michael@michaelauritt.com>
2016-06-11 12:39:02 -0400

    Fixed dict name in locations()

--- gelottery/flavors.py
+++ gelottery/flavors.py
@@ -11,7 +11,7 @@ location_numbers['CapoPenn'] = '4'
 
 def locations():
     """Print location names"""
-    for location in locations.keys():
+    for location in location_numbers.keys():
         print(location)
 
 def get_flavor_html(location):
